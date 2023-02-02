98b081d0f78f2c66881a11bdbc0108518a6dec52
Oluwatoyin John Adebusola <101136827+johbadebusola@users.noreply.github.com>
2023-02-02 22:53:52 +0100

    Added the various components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
-
+import PersonalInfo from "./assets/components/PersonalInfo"
+import SelectPlan from "./assets/components/SelectPlan"
+import "./assets/css/sidebar.css"
+import PickAddon from "./assets/components/PickAddon"
 function App() {
+  const [component] = useState(
+    [
+<PersonalInfo/>,<SelectPlan/>,<PickAddon/>
+    ]
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       
-      </header>
+    <div className='container' >
+      <div className='box1' >
+
+      </div>
+      <div className='box2' >
+
+      <div>
+        {component[1]}
+      </div>
+        <footer className='footer'>
+        <button className='button1'> Go Back</button>
+        <button className='button2'> Next Step</button>
+        </footer>
+      </div>
+
+
     </div>
   );
 }
---
+++ src/assets/components/PickAddon.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PickAddon = () => {
+  return (
+    <div>PickAddon</div>
+  )
+}
+
+export default PickAddon
