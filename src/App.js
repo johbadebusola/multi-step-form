201c6b06b73f29b3c3c06cadd2b7e109fa674fb8
Oluwatoyin John Adebusola <101136827+johbadebusola@users.noreply.github.com>
2023-02-09 20:33:14 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,139 +1,101 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import "./assets/css/formStep.css"
 import PersonalInfo from "./assets/components/PersonalInfo"
 import SelectPlan from "./assets/components/SelectPlan"
 import "./assets/css/sidebar.css"
 import PickAddon from "./assets/components/PickAddon"
-import Testform from './assets/components/Testform';
-import Testform2 from './assets/components/Testform2';
+
 
 function App() {
-   
-    const [formValue,setFormValue] = useState({
-      username: "",
-      email: "",
-      phoneNo: ""
-    })
 
-    const [active, setActive] = useState(false)
+  // State for Holding the form input information
+  const [formValue, setFormValue] = useState({
+    username: "",
+    email: "",
+    phoneNo: ""
+  })
 
-    const [component] = useState(
-      [
-        <PersonalInfo formValue={formValue} setFormValue={setFormValue} active={active} setActive={setActive} />, <SelectPlan />, <PickAddon />
-      ]
-    )
+  const [active, setActive] = useState(false)
+  const [toggle, setToggle] = useState(false)
 
-    const PageToggle = () => {
-        if (num === 1) {
-          return  <PersonalInfo formValue={formValue} setFormValue={setFormValue} active={active} setActive={setActive} />
-        }
-        else if (num === 2) {
-          return <SelectPlan />
-        }
-        else {
-          return <PickAddon />
-        }
-      }
-    const [num, setNum] = useState(1)
-  
+  const [component] = useState(
+    [
+      <PersonalInfo formValue={formValue} setFormValue={setFormValue} active={active} setActive={setActive} />, <SelectPlan toggle={toggle} setToggle={setToggle} />, <PickAddon />
+    ]
+  )
 
 
+  // FOR CYCLING THROUGH THE PAGES
+  const PageToggle = () => {
+    if (num === 1) {
+      return <PersonalInfo formValue={formValue} setFormValue={setFormValue} active={active} setActive={setActive} />
+    }
+    else if (num === 2) {
+      return <SelectPlan toggle={toggle} setToggle={setToggle}  />
+    }
+    else {
+      return <PickAddon />
+    }
+  }
+  const [num, setNum] = useState(1)
 
-    const next = (e) => {
-      if (num === component.length) {
-        return
-      }
 
-      if (formValue.phoneNo.trim() === "") {
-      
-        setActive(false)
-    
-        return
-      }
-      console.log(formValue.username)
-      setActive(true)
-   
-      setNum(num + 1)
+
+// for next button
+  const next = (e) => {
+    if (num === component.length) {
+      return
     }
 
-   
-    const prev = () => {
-      setNum(num - 1)
-      setFormValue({ ...formValue, username: "kfkfk" })
+    if (formValue.phoneNo.trim() === "") {
 
+      setActive(false)
+
+      return
     }
+
+    setActive(true)
+
+    setNum(num + 1)
+  }
+
+// for previous button
+  const prev = () => {
+    setNum(num - 1)
+    setFormValue({ ...formValue, username: "kfkfk" })
+
+  }
 
   console.log(active)
-    return (
-      <div className='container' >
-        <div className='box1' >
-          {/* <pre> {JSON.stringify(formValue,undefined,2)}</pre> */}
 
+  return (
+    <div className='container' >
+      <div className='box1' >
+        {/* <pre> {JSON.stringify(formValue,undefined,2)}</pre> */}
+        <div className='formStep' >
+          <div className={num >= 1 ? "completed" : ""} > 1 </div>
+          <div className={num >= 2 ? "completed" : ""}> 2 </div>
+          <div className={num >= 3 ? "completed" : ""}> 3 </div>
+          <div className={num === 4 ? "completed" : ""} > 4 </div>
         </div>
-        <div className='box2' >
-
-          <div>
-            {/* {PageToggle()} */}
-            <SelectPlan />
-          </div>
-          <footer className='footer'>
-            <button onClick={prev} className='button1' disabled={num === 1}> Go Back   </button>
-            <button onClick={next} className='button2'> Next Step</button>
-          </footer>
-        </div>
-
-
       </div>
+      <div className='box2' >
+
+
+        <div>
+          {PageToggle()}
+          {/* <SelectPlan toggle={toggle} setToggle={setToggle} /> */}
+        </div>
+        <footer className='footer'>
+          <button onClick={prev} className='button1' disabled={num === 1}> Go Back   </button>
+          <button onClick={next} className='button2'> Next Step</button>
+        </footer>
+      </div>
+
+
+    </div>
   );
-
-  // const [data, setData] = useState({
-  //   username: "",
-  //   email: ""
-  // })
-  // const [active, setActive] = useState()
-
-
-  // const PageToggle = () => {
-  //   if (num === 1) {
-  //     return <Testform data={data}  setData={setData} active={active} />
-  //   }
-  //   else if (num === 2) {
-  //     return <Testform2  />
-  //   }
-  // }
-
-  
-  // const [num, setNum] = useState(1)
-
-  // const next = () => {
-  //   if (data.username.length === 0) {
-  //     setActive(false)
-  //     return
-  //   }
-  //   if (data.email.length === 0) {
-  //     setActive(false)
-  //     return
-  //   }
-  //   setNum(num + 1)
-  //   setActive(true)
-
-  // }
-
-  // const prev = () => {
-  //       setNum(num - 1)
-        
-  //     }
-  // console.log(active)
-  // return (
-  //   <div>
-  //     <pre> {JSON.stringify(data, undefined, 1)}</pre>
-  //     {/* <Testform data={data} setData={setData} active={active} /> */}
-  //     {PageToggle()}
-
-  //     <button onClick={prev} className='button2'> Prev </button>
-  //     <button onClick={next} className='button2'> Next Step</button>
-  //   </div>
-  // )
 }
 
 export default App;
--- src/assets/components/SelectPlan.js
+++ src/assets/components/SelectPlan.js
@@ -1,39 +1,100 @@
-import React,{useState} from 'react'
+import React from 'react'
 import "../css/personalInfo.css"
 import "../css/selectPlan.css"
-const SelectPlan = () => {
- 
-  return(
-    <div >
-   <MonthlyPlan />
-      </div>
+import arcade from "../images/arcade.svg"
+import advance from "../images/advance.svg"
+import pro from "../images/pro.svg"
+const SelectPlan = ({toggle,setToggle}) => {
+const togg = (e) =>{
+setToggle(!toggle)
+}
+console.log(toggle)
+  return (
+    <div className='select-plan-container'>
+     
+      {!toggle?( <MonthlyPlan />): (<YearlyPlan />) }
+      <input 
+      type="checkbox"
+      value={toggle}
+      onClick={togg}
+      />
+    </div>
   )
 }
 
 
 
 const MonthlyPlan = () => {
-  return(
-    <div className='select-plan-container'>
-   <h2> Select your plan</h2>
-      <p> You have the option of monthly or yearly billing</p>
+  return (
+    <div className='monthly'>
+      <h2> Select your plan</h2>
+      <p> You have the option of monthly or yearly billing.</p>
 
-      <div> 
-<p>Arcade</p>
-<p>$9/mo</p>
-      </div>
-
-      <div> 
-      <p>Advance</p>
-<p>$12/mo</p>
-     
+      <div className='box' >
+        <img src={arcade} alt="arcade" />
+        <div>
+          <p className='p1'>Arcade</p>
+          <p className='p2'>$9/mo</p>
         </div>
 
-        <div> 
-        <p>Pro</p>
-<p>$15/mo</p>
+      </div>
+
+      <div className='box'>
+        <img src={advance} alt="advance" />
+        <div>
+          <p className='p1'>Advanced</p>
+          <p className='p2'>$12/mo</p>
         </div>
       </div>
+
+      <div className='box'>
+        <img src={pro} alt="pro" />
+        <div>
+          <p className='p1'>Pro</p>
+          <p className='p2'>$15/mo</p>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+
+const YearlyPlan = () => {
+  return (
+    <div className='monthly'>
+      <h2> Select your plan</h2>
+      <p> You have the option of monthly or yearly billing.</p>
+
+      <div className='box' >
+        <img src={arcade} alt="arcade" />
+        <div>
+          <p className='p1'>Arcade</p>
+          <p className='p2'>$90/yr</p>
+          <p className='p3'>2 months free</p>
+        </div>
+
+      </div>
+
+      <div className='box'>
+        <img src={advance} alt="advance" />
+        <div>
+          <p className='p1'>Advanced</p>
+          <p className='p2'>$120/yr</p>
+          <p className='p3'>2 months free</p>
+        </div>
+      </div>
+
+      <div className='box'>
+        <img src={pro} alt="pro" />
+        <div>
+          <p className='p1'>Pro</p>
+          <p className='p2'>$150/yr</p>
+          <p className='p3'>2 months free</p>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
